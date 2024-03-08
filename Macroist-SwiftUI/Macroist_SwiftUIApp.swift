8d2bdd1b5cb206020b73ed08398b18f05e87e125
Ryan Schildknecht <schildknecht618@gmail.com>
2024-03-08 16:32:38 -0600

    Initial Commit

---
+++ Macroist-SwiftUI/Macroist_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Macroist_SwiftUIApp.swift
+//  Macroist-SwiftUI
+//
+//  Created by Ryan Schildknecht on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Macroist_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
